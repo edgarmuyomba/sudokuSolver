4c9b21d51a23fdd96d144de9cafa6ff9f052b0a6
edgarmuyomba <edgarmatthew247@gmail.com>
2024-06-15 12:22:44 +0300

    wip: grid styles

--- src/components/grid/grid.tsx
+++ src/components/grid/grid.tsx
@@ -5,6 +5,17 @@ export default function Grid() {
         <div className={styles.container}>
             <div className={styles.group}>
                 <div className={styles.cell}>1</div>
+                <div className={`${styles.cell} ${styles.init}`}>2</div>
+                <div className={styles.cell}>3</div>
+                <div className={styles.cell}>4</div>
+                <div className={styles.cell}>5</div>
+                <div className={`${styles.cell} ${styles.init}`}>6</div>
+                <div className={styles.cell}>7</div>
+                <div className={styles.cell}>8</div>
+                <div className={styles.cell}>9</div>
+            </div>
+            <div className={styles.group}>
+                <div className={styles.cell}>1</div>
                 <div className={styles.cell}>2</div>
                 <div className={styles.cell}>3</div>
                 <div className={styles.cell}>4</div>
@@ -13,6 +24,28 @@ export default function Grid() {
                 <div className={styles.cell}>7</div>
                 <div className={styles.cell}>8</div>
                 <div className={styles.cell}>9</div>
+            </div>
+            <div className={styles.group}>
+                <div className={styles.cell}>1</div>
+                <div className={styles.cell}>2</div>
+                <div className={styles.cell}>3</div>
+                <div className={`${styles.cell} ${styles.init}`}>4</div>
+                <div className={styles.cell}>5</div>
+                <div className={styles.cell}>6</div>
+                <div className={styles.cell}>7</div>
+                <div className={styles.cell}>8</div>
+                <div className={`${styles.cell} ${styles.init}`}>9</div>
+            </div>
+            <div className={styles.group}>
+                <div className={styles.cell}>1</div>
+                <div className={styles.cell}>2</div>
+                <div className={styles.cell}>3</div>
+                <div className={`${styles.cell} ${styles.valid}`}>4</div>
+                <div className={`${styles.cell} ${styles.init}`}>5</div>
+                <div className={styles.cell}>6</div>
+                <div className={styles.cell}>7</div>
+                <div className={styles.cell}>8</div>
+                <div className={`${styles.cell} ${styles.valid}`}>9</div>
             </div>
             <div className={styles.group}>
                 <div className={styles.cell}>1</div>
@@ -41,7 +74,7 @@ export default function Grid() {
                 <div className={styles.cell}>2</div>
                 <div className={styles.cell}>3</div>
                 <div className={styles.cell}>4</div>
-                <div className={styles.cell}>5</div>
+                <div className={`${styles.cell} ${styles.init}`}>5</div>
                 <div className={styles.cell}>6</div>
                 <div className={styles.cell}>7</div>
                 <div className={styles.cell}>8</div>
@@ -51,54 +84,21 @@ export default function Grid() {
                 <div className={styles.cell}>1</div>
                 <div className={styles.cell}>2</div>
                 <div className={styles.cell}>3</div>
-                <div className={styles.cell}>4</div>
+                <div className={`${styles.cell} ${styles.valid}`}>4</div>
                 <div className={styles.cell}>5</div>
-                <div className={styles.cell}>6</div>
+                <div className={`${styles.cell} ${styles.invalid}`}>6</div>
                 <div className={styles.cell}>7</div>
                 <div className={styles.cell}>8</div>
                 <div className={styles.cell}>9</div>
             </div>
             <div className={styles.group}>
-                <div className={styles.cell}>1</div>
+                <div className={`${styles.cell} ${styles.valid}`}>1</div>
                 <div className={styles.cell}>2</div>
                 <div className={styles.cell}>3</div>
-                <div className={styles.cell}>4</div>
+                <div className={`${styles.cell} ${styles.invalid}`}>4</div>
                 <div className={styles.cell}>5</div>
                 <div className={styles.cell}>6</div>
-                <div className={styles.cell}>7</div>
-                <div className={styles.cell}>8</div>
-                <div className={styles.cell}>9</div>
-            </div>
-            <div className={styles.group}>
-                <div className={styles.cell}>1</div>
-                <div className={styles.cell}>2</div>
-                <div className={styles.cell}>3</div>
-                <div className={styles.cell}>4</div>
-                <div className={styles.cell}>5</div>
-                <div className={styles.cell}>6</div>
-                <div className={styles.cell}>7</div>
-                <div className={styles.cell}>8</div>
-                <div className={styles.cell}>9</div>
-            </div>
-            <div className={styles.group}>
-                <div className={styles.cell}>1</div>
-                <div className={styles.cell}>2</div>
-                <div className={styles.cell}>3</div>
-                <div className={styles.cell}>4</div>
-                <div className={styles.cell}>5</div>
-                <div className={styles.cell}>6</div>
-                <div className={styles.cell}>7</div>
-                <div className={styles.cell}>8</div>
-                <div className={styles.cell}>9</div>
-            </div>
-            <div className={styles.group}>
-                <div className={styles.cell}>1</div>
-                <div className={styles.cell}>2</div>
-                <div className={styles.cell}>3</div>
-                <div className={styles.cell}>4</div>
-                <div className={styles.cell}>5</div>
-                <div className={styles.cell}>6</div>
-                <div className={styles.cell}>7</div>
+                <div className={`${styles.cell} ${styles.valid}`}>7</div>
                 <div className={styles.cell}>8</div>
                 <div className={styles.cell}>9</div>
             </div>
